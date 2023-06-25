3b63e6c89c0b1d6ab956243263b7419568679f8e
Neo Nguyen <neonguyen.ca@gmail.com>
2023-06-24 18:56:54 -0600

    connect to database successful

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,20 @@
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+import db from '../models/index';
+
+let getHomePage = async (req, res) => { //async: đồng bộ
+    try {
+        let data = await db.User.findAll();
+        //await phải được map theo Model name
+        //User ko có s bởi vì trong file user thì User là của Model Name chứ ko phải users trong migration
+        console.log('==================')
+        console.log(data)
+        console.log('==================')
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e)
+    }
+
 }
 
 let getAboutPage = (req, res) => {
